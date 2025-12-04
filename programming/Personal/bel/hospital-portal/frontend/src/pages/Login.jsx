5fc0a347d1939e3a7feb3c199fba77d97bceea3c
HimAnshNA <108965166+HimAnshNA@users.noreply.github.com>
2025-12-04 09:01:37 +0530

    Update Login.jsx
    connected render

--- programming/Personal/bel/hospital-portal/frontend/src/pages/Login.jsx
+++ programming/Personal/bel/hospital-portal/frontend/src/pages/Login.jsx
@@ -36,7 +36,7 @@ export default function Login() {
 
     try {
       const res = await axios.post(
-        "http://localhost:5000/api/auth/login",
+        "https://healcareportal.onrender.com/api/auth/login",
         form
       );
 
